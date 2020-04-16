3a10fa79a4a92bce0698ca18399f3ca71f767213
Avinash <akumart11@gmail.com>
2020-04-16 12:30:22 +0530

    Category Service and Category Service Test Impl

---
+++ src/main/java/avinash/springframework/spring5mvcrest/services/CategoryService.java
@@ -0,0 +1,13 @@
+package avinash.springframework.spring5mvcrest.services;
+
+import java.util.List;
+
+import avinash.springframework.spring5mvcrest.api.v1.domain.CategoryDTO;
+import avinash.springframework.spring5mvcrest.domain.Category;
+
+public interface CategoryService {
+	
+	List<CategoryDTO> getAllCategories();
+	CategoryDTO getCategoryByName(String name);
+
+}
